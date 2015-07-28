08f4f758a0d339d61d5bbf81c32e101b5e59964d
0Cristofer <cristoferoswald@gmail.com>
2015-07-28 17:10:23 -0300

    mudanças grandes

--- game.py
+++ game.py
@@ -4,20 +4,35 @@ import palavras
 from random import randint
 from google.appengine.ext import ndb
 
-class GameState(ndb.Model):
+#Classes no Ndb
+#Classes para GameStates =======================================
+class GameStates(ndb.Model):
+    PreState = ndb.BooleanProperty(indexed=False, default=False)
     State = ndb.BooleanProperty(indexed=False, default=False)
 
-def setGame(chat_id, bool):
-    es = GameState.get_or_insert(str(chat_id))
-    es.State = bool
+def setPreGame(chat_id, status):
+    es = GameStates.get_or_insert(str(chat_id))
+    es.PreState = status
     es.put()
 
-def getGame(chat_id):
-    es = GameState.get_by_id(str(chat_id))
+def getPreGame(chat_id):
+    es = GameStates.get_by_id(str(chat_id))
+    if es:
+        return es.PreState
+    return False
+
+def setInGame(ndb.Model,status):
+    es = GameStates.get_or_insert(str(chat_id))
+    es.State = status
+    es.put()
+
+def getInGame(ndb.Model):
+    es = GameStates.get_by_id(str(chat_id))
     if es:
         return es.State
     return False
 
+#Classes para lista de jogares ==================================
 class Players(ndb.Model):
     jogadores = [ndb.StringProperty(indexed=False, default=False)]
     nomes = [ndb.StringProperty(indexed=False, default=False)]
@@ -46,41 +61,50 @@ class Jogo:
 
     def comandos(self, uId, uName, chat_id, text):
         rpl = []
-        state = getGame(chat_id)
-        if text.startswith('/'):
-            if text.startswith('/novojogo') or text.startswith('/novojogo@forca_bot'):
-                if state == False:
-                    str1 = 'Comecando um novo jogo! Voce sera o administrador dessa rodada '+uName
-                    setGame(chat_id,True)
-                    str2 = 'Vamos comecar definindo os jogadores\nQuem quiser participar dessa rodada envie um /entrar :D'
-                    str3 = 'Para fechar o grupo de participantes mande um /fechar Administador'
-                    rpl = [str1, str2, str3]
-                    #Continuar
-                else:
-                    str1 = 'Existe um jogo em andamento neste chat!\nCaso voce queira abandonar ele use o comando /cancelar'
+        PreState = getPreGame(chat_id)
+        State = getInGame(chat_id)
+        if (State == False):
+            if text.startswith('/'):
+                if text.startswith('/novojogo') or text.startswith('/novojogo@forca_bot'):
+                    if PreState == False:
+                        str1 = 'Comecando um novo jogo! Voce sera o administrador dessa rodada '+uName
+                        setGame(chat_id,True)
+                        str2 = 'Vamos comecar definindo os jogadores\nQuem quiser participar dessa rodada envie um /entrar :D'
+                        str3 = 'Para fechar o grupo de participantes mande um /fechar Administador'
+                        addPlayer(chat_id, uId, uName)
+                        rpl = [str1, str2, str3]
+                        #Continuar
+                    else:
+                        str1 = 'Existe um jogo sendo organizado!\nCaso voce queira abandonar ele use o comando /cancelar'
+                        rpl = [str1]
+                elif text.startswith('/entrar') or text.startswith('/entrar@forca_bot'):
+                    addPlayer(chat_id, uId, uName)
+                    str1 = 'Certo, '+uName+' voce vai participar desta rodada'
                     rpl = [str1]
-            elif text.startswith('/entrar') or text.startswith('/entrar@forca_bot'):
-                addPlayer(chat_id, uId, uName)
-                str1 = 'Certo, '+uName+' voce vai participar desta rodada'
-                rpl = [str1]
-            elif text.startswith('/fechar') or text.startswith('/fechar@forca_bot'):
-                nomes = getPlayers(chat_id)
-                str1 = 'Grupo de participantes fechados! Jogarao nesta rodada:'
-                rpl = [str1]
-                for i in range(1,len(nomes)):
-                    rpl.append(nomes[i])
-            elif text.startswith('/cancelar') or text.startswith('/cancelar@forca_bot'):
-                if state:
-                    str1 = 'Voce cancelou o jogo' #implementar cancelamento por votacao
-                    setGame(chat_id,False)
+                elif text.startswith('/fechar') or text.startswith('/fechar@forca_bot'):
+                    nomes = getPlayers(chat_id)
+                    str1 = 'Grupo de participantes fechados! Jogarao nesta rodada:'
+                    rpl = [str1]
+                    for i in range(1,len(nomes)):
+                        rpl.append(nomes[i])
+                    rpl.append('O jogo vai comecar agora!') #Mudar talvez
+                elif text.startswith('/cancelar') or text.startswith('/cancelar@forca_bot'):
+                    if PreState:
+                        str1 = 'Voce cancelou o jogo' #implementar cancelamento por votacao
+                        setGame(chat_id,False)
+                    else:
+                        str1 = 'Nao existe nenhum jogo ativo! Comece um com o comando /novojogo'
+                    rpl = [str1]
+                elif text.startswith('/help') or text.startswith('/help@forca_bot'):
+                    str1 = 'Sou o Forca_bot, para comecar um jogo use o comando /newgame\nUse este comando /help e irei te guiando!'
+                    rpl = [str1]
                 else:
-                    str1 = 'Nao existe nenhum jogo ativo! Comece um com o comando /novojogo'
-                rpl = [str1]
-            elif text.startswith('/help') or text.startswith('/help@forca_bot'):
-                str1 = 'Sou o Forca_bot, para comecar um jogo use o comando /newgame\nUse este comando /help e irei te guiando!'
-                rpl = [str1]
+                    rpl = ['Comando nao reconhecido']
             else:
-                rpl = ['Comando nao reconhecido']
-        else:
-            rpl = ['Nao eh um comando, lembre se que comandos comecam com /']
+                rpl = ['Nao eh um comando, lembre se que comandos comecam com /']
+            return rpl
+        else: #Existe um jogo em andamento
+            if text.startswith('/cancelar') or text.startswith('/cancelar@forca_bot'): #Mudar, sei la
+                str1 = 'O jogo que estava em andamento foi cancelado!'
+                rpl = [str1]
         return rpl
